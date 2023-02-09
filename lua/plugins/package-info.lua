2d69c3c39f3886d2f015a495a2d13fdca1936313
Verlon Smith <vsmith@alteryx.com>
2023-02-08 17:01:55 -0700

    Added package-info plugin

---
+++ lua/plugins/package-info.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "vuki656/package-info.nvim",
+    config = function()
+      require("package-info").setup()
+    end,
+    lazy = true,
+    event = { "BufReadPre", "BufNew" },
+  },
+}
