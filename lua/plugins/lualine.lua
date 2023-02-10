e05bea2086b3752723932734086dc443fce4e3bb
Verlon Smith <verlonsmith@Verlons-MacBook-Pro.local>
2023-02-09 21:58:44 -0700

    Adjusted color

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -74,8 +74,8 @@ return {
             -- We are going to use lualine_c an lualine_x as left and
             -- right section. Both are highlighted by c theme .  So we
             -- are just setting default looks o statusline
-            normal = { c = { fg = colors.fg, bg = colors.base2 } },
-            inactive = { c = { fg = colors.fg, bg = colors.base2, whhblue } },
+            normal = { c = { fg = colors.fg, bg = colors.base1 } },
+            inactive = { c = { fg = colors.fg, bg = colors.base1 } },
           },
           always_divide_middle = true,
         },
