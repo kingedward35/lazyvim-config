da49a21fbf10d54205f05050fb70b579fabdc66e
Verlon Smith <vsmith@alteryx.com>
2023-05-08 08:45:03 -0600

    Updated dependencies

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -76,7 +76,7 @@ return {
           "<cmd>Telescope lsp_workspace_diagnostics<cr>",
           "Workspace Diagnostics",
         },
-        f = { "<cmd>lua vim.lsp.buf.format()<cr>", "Format" },
+        f = { "<cmd>lua vim.lsp.buf.format({ timeout = 2000 })<cr>", "Format" },
         i = { "<cmd>LspInfo<cr>", "Info" },
         I = { "<cmd>LspInstallInfo<cr>", "Installer Info" },
         j = {
