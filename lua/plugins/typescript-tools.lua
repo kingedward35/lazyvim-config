83e2b09017d7733f06d521ce0fc2cd29f4199134
Verlon Smith <vsmith@alteryx.com>
2023-08-17 15:33:59 -0600

    Added inlay hints

--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -1,5 +1,16 @@
 return {
   "pmizio/typescript-tools.nvim",
   dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-  opts = {},
+  opts = {
+    settings = {
+      tsserver_file_preferences = {
+        includeInlayParameterNameHints = "all",
+        includeInlayEnumMemberValueHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayVariableTypeHints = true,
+      },
+    },
+  },
 }
