57e86ff9a500d89e89539ff55b5aa7155406e41d
Verlon Smith <vsmith@alteryx.com>
2023-10-12 12:16:47 -0600

    Added chat-gpt dependency

--- lua/plugins/chat-gpt.lua
+++ lua/plugins/chat-gpt.lua
@@ -2,11 +2,16 @@ return {
   "jackMort/ChatGPT.nvim",
   event = "VeryLazy",
   config = function()
-    require("chatgpt").setup()
+    require("chatgpt").setup({
+      openai_params = {
+        max_tokens = 3000,
+      },
+    })
   end,
   dependencies = {
     "MunifTanjim/nui.nvim",
     "nvim-lua/plenary.nvim",
     "nvim-telescope/telescope.nvim",
   },
+  commit = "d4aa4d9e31d620a0c01006e59f4449ffc7eb33ce",
 }
