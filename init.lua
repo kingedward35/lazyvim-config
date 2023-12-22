4636fcf1b10cc03deb8c04113b4959ac8d3342f2
Verlon Smith <vsmith@alteryx.com>
2023-12-22 12:42:26 -0700

    Updated dependencies

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+-- vscode and nvim only settings
+if vim.g.vscode then
+  -- VSCode extension
+
+  -- map keyboard quickfix
+  nnoremap("z=", "<Cmd>call VSCodeNotify('keyboard-quickfix.openQuickFix')<Cr>")
+else
+  -- ordinary neovim
+end
