0c7deca4f52fde20465ff6b430022ed098db1ab6
Verlon Smith <vsmith@alteryx.com>
2024-02-25 16:15:58 -0700

    Added new colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,61 @@
 return {
-  "LazyVim/LazyVim",
-  opts = {
-    -- colorscheme = "zephyr",
-    -- colorscheme = "tokyonight",
-    colorscheme = "tokyodark",
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "zephyr",
+      -- colorscheme = "tokyonight",
+      colorscheme = "tokyodark",
+      -- colorscheme = "catppuccin",
+    },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = true,
+    opts = {
+      flavour = "mocha",
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        },
+      },
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    config = function()
+      require("tokyonight").setup({
+        style = "night",
+        on_colors = function(colors)
+          colors.bg_dark = "#000000"
+          colors.bg = "#000000"
+        end,
+      })
+    end,
+  },
+  {
+    "tiagovla/tokyodark.nvim",
+    lazy = true,
+    opts = {
+      custom_palette = {
+        black = "#000000",
+        bg0 = "#000000",
+      },
+      styles = {
+        comments = { italic = true }, -- style for comments
+        keywords = { italic = true }, -- style for keywords
+        identifiers = { italic = true }, -- style for identifiers
+        functions = {}, -- style for functions
+        variables = {}, -- style for variables
+      },
+    },
+  },
+  {
+    "kingedward35/zephyr-nvim",
+    lazy = true,
   },
   --  config = function()
   --    -- vim.g.everforest_background = "hard"
