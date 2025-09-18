53598889417efdcb1db3ef17de6fc92090de2e73
Verlon <vsmith@alteryx.com>
2025-09-18 09:30:52 -0600

    Updated some configs

--- init.lua
+++ init.lua
@@ -13,7 +13,38 @@ if vim.g.vscode then
   -- VSCode extension
 
   -- map keyboard quickfix
-  nnoremap("z=", "<Cmd>call VSCodeNotify('keyboard-quickfix.openQuickFix')<Cr>")
+  -- nnoremap("z=", "<Cmd>call VSCodeNotify('keyboard-quickfix.openQuickFix')<Cr>")
+  -- vim.g.mapleader = ","
+  local map = function(mode, lhs, cmd)
+    vim.keymap.set(mode, lhs, function()
+      vim.fn.VSCodeNotify(cmd)
+    end, { silent = true })
+  end
+
+  vim.keymap.set("n", "<C-d>", "<C-d>zz", { silent = true })
+  vim.keymap.set("n", "<C-u>", "<C-u>zz", { silent = true })
+
+  map("n", "<C-h>", "workbench.action.focusLeftGroup")
+  map("n", "<C-j>", "workbench.action.focusBelowGroup")
+  map("n", "<C-k>", "workbench.action.focusAboveGroup")
+  map("n", "<C-l>", "workbench.action.focusRightGroup")
+
+  -- “Leader” actions
+  -- map("n", ",f", "workbench.action.quickOpen")
+  -- map("n", ",m", "workbench.action.findInFiles")
+  map("n", ",e", "workbench.view.explorer")
+  map("n", ",t", "workbench.action.terminal.toggleTerminal")
+  map("n", ",q", "workbench.action.closeActiveEditor")
+  map("<D-r>", "workbench.action.reloadWindow")
+  map("<leader>e", "workbench.action.toggleSidebarVisibility")
+  -- ,w → save file
+  map("<leader>w", "workbench.action.files.save")
+  -- ,c → close editor
+  map("<leader>c", "workbench.action.closeActiveEditor")
+  -- ,m → quick open (file search)
+  map("<leader>m", "workbench.action.quickOpen")
+  -- ,st → search across project
+  map("<leader>st", "workbench.action.findInFiles")
 else
   -- ordinary neovim
 end
