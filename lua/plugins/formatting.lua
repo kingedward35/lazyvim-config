e13646d1cdb793af6365add96a81ffc220df5556
Verlon <vsmith@alteryx.com>
2025-12-11 12:40:32 -0700

    Updated formatting config

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -3,12 +3,6 @@ return {
   enabled = true,
   event = { "BufReadPre", "BufNewFile" },
   opts = {
-    -- format_on_save = {
-    --   timeout_ms = 10000,
-    --   async = false, -- not recommended to change
-    --   quiet = false, -- not recommended to change
-    --   lsp_fallback = true, -- not recommended to change
-    -- },
     formatters_by_ft = {
       javascript = { "eslint" },
       typescript = { "eslint" },
